53eb9da71bc0320338543cd9d0ae1fc2434bc776
ppoohh25 <poohrocker25@gmail.com>
2021-12-24 10:54:16 +0700

    button update

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,10 +1,10 @@
 const {SlashCommandBuilder} = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js')
+const { MessageEmbed, MessageActionRow,MessageButton} = require('discord.js')
 
 const botinfoEmbed = new MessageEmbed()
          .setColor('#EB3333')
          .setTitle(':robot:** Bot Info**')
-         .setDescription('**Just a test bot that Developed by Sadayu**'+
+         .setDescription('**A lovely bot that Developed by Sadayu**'+
                          '\n'+'**Contact**'+
                          '\n'+':link:FB :'+
                          '\n'+'https://bit.ly/3FqkREL'+
@@ -13,12 +13,31 @@ const botinfoEmbed = new MessageEmbed()
                          '\n'+':link:Github :'+
                          '\n'+'https://gist.github.com/ppoohh25'
                          )
+const row = new MessageActionRow()
+    .addComponents(
+    new MessageButton()
+    .setURL('https://bit.ly/3FqkREL')
+    .setLabel('Facebook')
+    .setStyle('LINK')
+    )
+    .addComponents(
+        new MessageButton()
+        .setURL('https://bit.ly/3HAgx7J')
+        .setLabel('Instagram')
+        .setStyle('LINK')
+    )
+    .addComponents(
+        new MessageButton()
+        .setURL('https://gist.github.com/ppoohh25')
+        .setLabel('Github')
+        .setStyle('LINK')
+    )
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('botinfo')
 		.setDescription('Give you this bot info'),
 	async execute(interaction) {
-		return interaction.reply({embeds:[botinfoEmbed]});
+		return interaction.reply({embeds:[botinfoEmbed], components:[row]});
 	},
 };
--- prefix_command/botinfo.js
+++ prefix_command/botinfo.js
@@ -1,9 +1,9 @@
-const {MessageEmbed} = require('discord.js')
+const {MessageEmbed, MessageActionRow, MessageButton} = require('discord.js')
 
 const botinfoEmbed = new MessageEmbed()
         .setColor('#EB3333')
         .setTitle(':robot:** Bot Info**')
-        .setDescription('**Just a test bot that Developed by Sadayu**'+
+        .setDescription('**A lovely bot that Developed by Sadayu**'+
                         '\n'+'**Contact**'+
                         '\n'+':link:FB :'+
                         '\n'+'https://bit.ly/3FqkREL'+
@@ -12,9 +12,30 @@ const botinfoEmbed = new MessageEmbed()
                         '\n'+':link:Github :'+
                         '\n'+'https://gist.github.com/ppoohh25'
                         )
+
+const row = new MessageActionRow()
+    .addComponents(
+    new MessageButton()
+    .setURL('https://bit.ly/3FqkREL')
+    .setLabel('Facebook')
+    .setStyle('LINK')
+    )
+    .addComponents(
+        new MessageButton()
+        .setURL('https://bit.ly/3HAgx7J')
+        .setLabel('Instagram')
+        .setStyle('LINK')
+    )
+    .addComponents(
+        new MessageButton()
+        .setURL('https://gist.github.com/ppoohh25')
+        .setLabel('Github')
+        .setStyle('LINK')
+    )
+
 module.exports = {
     name:'botinfo',
     execute(msg, args){
-        msg.channel.send({ embeds: [botinfoEmbed] })
+        msg.channel.send({ embeds: [botinfoEmbed] , components:[row]})
     }
 }
