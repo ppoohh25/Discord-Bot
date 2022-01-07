c0fcfa82a5670769abb284ae92b89c5c448d2a84
ppoohh25 <poohrocker25@gmail.com>
2022-01-07 10:53:46 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -458,12 +458,12 @@ module.exports = {
         timezone: 'Asia/Bangkok'
     })
 // test
-    // cron.schedule('0-59 * * * * *', async ()=>{
-    //     await channel.send('Test')
-    // },{
-    //     scheduled: true,
-    //     timezone: 'Asia/Bangkok'
-    // })
+    cron.schedule('0-59 * * * * *', async ()=>{
+        await channel.send('Test')
+    },{
+        scheduled: true,
+        timezone: 'Asia/Bangkok'
+    })
 // test
 
 	},
